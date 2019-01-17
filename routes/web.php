23280dfd91bd0ad51fb17b564cfa9912e102c3b9
Thomas Fini Hansen <xen@xen.dk>
2019-01-17 13:17:37 +0100

    Add google auth and make home the default

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/redirect', 'Auth\LoginController@redirectToProvider')->name('redirect');
+Route::get('/callback', 'Auth\LoginController@handleProviderCallback');
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/', 'HomeController@index')->name('home');
