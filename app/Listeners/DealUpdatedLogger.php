5aef555045dc8afde3ec66927610929968768691
Thomas Fini Hansen <xen@xen.dk>
2019-01-25 14:20:52 +0100

    Add DealUpdatedLogger

---
+++ app/Listeners/DealUpdatedLogger.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\DealUpdated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class DealUpdatedLogger
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  DealUpdated  $event
+     * @return void
+     */
+    public function handle(DealUpdated $event)
+    {
+        Log::info(sprintf('Deal %d update', $event->dealId));
+    }
+}
---
+++ tests/Feature/DealUpdatedLoggerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Events\DealUpdated;
+use App\Listeners\DealUpdatedLogger;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Log;
+
+class DealUpdatedLoggerTest extends TestCase
+{
+    public function testLogging()
+    {
+        Log::shouldReceive('info')
+            ->once()
+            ->with('Deal 42 update')
+            ->andReturn();
+
+        $logger = new DealUpdatedLogger();
+        $logger->handle(new DealUpdated(42));
+    }
+}
